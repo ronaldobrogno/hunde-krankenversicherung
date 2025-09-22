8b0739e4a3db6f38ce10fda4fe7a20a19b608cd6
ronaldobrogno <ronaldobrogno@gmail.com>
2025-09-22 22:57:48 +0200

    Update site.js

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,8 +1,8 @@
 module.exports = {
-  name: "Hunde-Krankenversicherung Direkt",
-  url: "https://DEINE-NETLIFY-URL.netlify.app", // später ersetzen durch eigene Domain
+  name: "Hunde-Krankenversicherung",
+  url: "https://DEINE-NETLIFY-URL.netlify.app", // später deine eigene Domain
   description: "Hunde-Krankenversicherung mit OP-Schutz, Heilbehandlung & Vorsorge. Für jede Hunderasse eine eigene Seite – jetzt online abschließen.",
   locale: "de_DE",
-  ctaLink: "https://www.barmenia.de/de/produkte/tierversicherung/hunde-krankenversicherung",
+  ctaLink: "https://ssl.barmenia.de/online-versichern/#/tierversicherung/Beitrag?tierart=Hund&versicherung=kv&adm=00751613",
   year: new Date().getFullYear()
 };

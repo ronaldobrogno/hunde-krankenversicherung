5aa3399f4bdc38977e8cd9498bbd62cc5873bebc
ronaldobrogno <ronaldobrogno@gmail.com>
2025-09-22 14:30:34 +0200

    Create site.js

---
+++ src/_data/site.js
@@ -0,0 +1,7 @@
+module.exports = {
+  name: "Hunde-Krankenversicherung Direkt",
+  url: "", // später hier deine Domain eintragen, z. B. "https://hunde-krankenversicherung.de"
+  description: "Hunde-Krankenversicherung mit OP-Schutz, Heilbehandlung & Vorsorge. Für jede Hunderasse eine eigene Seite – jetzt online abschließen.",
+  locale: "de_DE",
+  ctaLink: "https://www.barmenia.de/de/produkte/tierversicherung/hunde-krankenversicherung" // dein Abschlusslink
+};
